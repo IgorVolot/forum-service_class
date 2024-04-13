b9a273cbef161c756413be313bca006aca365b80
Igor Volotovskyi <ihor737@gmail.com>
2024-04-13 20:53:10 +0200

    checkEndpoint method done, AuthorizationFilter added

---
+++ src/main/java/ait/cohort34/security/filter/AuthorizationFilter.java
@@ -0,0 +1,59 @@
+package ait.cohort34.security.filter;
+
+import ait.cohort34.accounting.dao.UserAccountRepository;
+import ait.cohort34.accounting.dto.exceptions.UserNotFoundException;
+import ait.cohort34.accounting.model.Role;
+import ait.cohort34.accounting.model.UserAccount;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@Component
+@RequiredArgsConstructor
+@Order(20)
+public class AuthorizationFilter implements Filter {
+    final UserAccountRepository userAccountRepository;
+    private static final Role ADMINISTRATOR_ROLE = Role.ADMINISTRATOR;
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        boolean isUserRoleEndpoint = request.getRequestURI().equals("/role/{role}");
+
+        if (isUserRoleEndpoint) {
+            try {
+                String authHeader = request.getHeader("Authorization");
+                if (authHeader == null || authHeader.isBlank()) {
+                    response.setStatus(401);
+                    return;
+                }
+                String base64Credentials = authHeader.substring("Basic".length()).trim();
+                byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
+                String[] credentials = new String(credDecoded, StandardCharsets.UTF_8).split(":");
+                UserAccount userAccount = userAccountRepository.findById(credentials[0]).orElseThrow(UserNotFoundException::new);
+
+                if (!userAccount.getPassword().equals(credentials[1])) {
+                    throw new UserNotFoundException();
+                }
+
+                if (userAccount.getRoles().stream().noneMatch(role -> role.equals(ADMINISTRATOR_ROLE))) {
+                    throw new UserNotFoundException();
+                }
+            } catch (Exception e) {
+                response.setStatus(403);
+                return;
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+}
+
