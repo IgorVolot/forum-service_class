ae8d17cf36ac4b335d21440d9f6fb0729af1beb6
Igor Volotovskyi <ihor737@gmail.com>
2024-04-16 13:05:59 +0200

    some Filters added.

--- src/main/java/ait/cohort34/security/filter/DeleteUserFilter.java
+++ src/main/java/ait/cohort34/security/filter/DeleteUserFilter.java
@@ -1,26 +1,29 @@
 package ait.cohort34.security.filter;
 
+import ait.cohort34.accounting.model.Role;
+import ait.cohort34.security.model.User;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
 @Order(40)
-public class OwnerManagingFilter implements Filter {
+public class DeleteUserFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
-
-        if (checkEndpoint(request.getMethod(), request.getServletPath())){
-            String login = request.getUserPrincipal().getName();
-
-            if(!login.matches(request.getRemoteUser())){
-                response.sendError(401, "Unauthorized");
+        if ( checkEndpoint(request.getMethod(), request.getServletPath()) ) {
+            User user = (User) request.getUserPrincipal();
+            String[] parts = request.getServletPath().split("/");
+            String owner = parts[parts.length - 1];
+            if ( !(user.getName().equalsIgnoreCase(owner)) || !user.getRoles().contains(Role.ADMINISTRATOR.name())) {
+                response.sendError(403, "Unauthorized");
                 return;
             }
         }
@@ -28,6 +31,6 @@ public class OwnerManagingFilter implements Filter {
     }
 
     private boolean checkEndpoint(String method, String path) {
-        return ("/account/user/\\w+".equalsIgnoreCase(path)) || ("/forum/post/\\w+".equalsIgnoreCase(path)) || ("/account/password".equalsIgnoreCase(path));
+        return (HttpMethod.DELETE.matches(method) && path.matches("/account/user/\\w+"));
     }
 }
